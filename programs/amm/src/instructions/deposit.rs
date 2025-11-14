81f4c4240c015b44b51f88b4015539c5104e0a1d
Vipul-045 <vipulyadav0479@gmail.com>
2025-11-14 15:49:31 +0530

    All accounts derived in  deposit instructions

--- programs/amm/src/instructions/deposit.rs
+++ programs/amm/src/instructions/deposit.rs
@@ -33,7 +33,7 @@ pub struct Deposit<'info>{
         seeds = [b"lp", config.key().as_ref()],
         bump = config.lp_bump,
     )]
-    pub mint_lp: Account<'info, Mint>
+    pub mint_lp: Account<'info, Mint>,
 
     #[account(
         mut,
@@ -51,5 +51,34 @@ pub struct Deposit<'info>{
     )]
     pub vault_y: Account<'info, TokenAccount>,
 
-    
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+        associated_token::token_program = token_program
+    )]
+    pub user_ata_x: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+        associated_token::token_program = token_program
+    )]
+    pub user_ata_y: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint_lp,
+        associated_token::authority = user,
+        associated_token::token_program = token_program
+    )]
+    pub user_ata_lp: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
+    pub system_program: Program<'info, System>,
 }
