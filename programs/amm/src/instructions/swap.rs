52159878d3d04b628bf2acaa54180dc427c0ba89
Vipul-045 <vipulyadav0479@gmail.com>
2025-11-17 15:35:17 +0530

    deposit fun in Swap fun completed

--- programs/amm/src/instructions/swap.rs
+++ programs/amm/src/instructions/swap.rs
@@ -96,3 +96,33 @@ pub fn Swap(&mut self, is_x: bool, amount_in:u64, min_amount_out: u64) -> Result
 
     Ok(())
 }
+
+pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result <()>{
+    let(from, to, mint, decimals) = match is_x {
+        true => (
+            self.user_ata_x.to_account_info(),
+            self.vault_x.to_account_info(),
+            self.mint_x.to_account_info(),
+            self.mint_x.decimals
+        ),
+        false => (
+            self.user_ata_y.to_account_info(),
+            self.vault_y,to_account_info(),
+            self.mint_y.to_account_info(),
+            self.mint_y.decimals()
+        )
+    };
+
+    let cpi_program = self.token_program.to_account_info();
+
+    let cpi_accounts = TransferChecked{
+        from, 
+        to,
+        mint,
+        decimals
+    };
+
+    let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+
+    transfer_checked(cpi_context, amount, decimals);
+}
