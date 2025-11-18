1d154a8631a09bd8d05d6c9a98ab6adfba61e361
Vipul-045 <vipulyadav0479@gmail.com>
2025-11-18 15:42:24 +0530

    Errors.rs done with all errors

---
+++ programs/amm/src/errors.rs
@@ -0,0 +1,42 @@
+use anchor_lang::prelude::*;
+use constant_product_curve::CurveError;
+
+#[error_code]
+pub enum AmmError{
+    #[msg("Default error")]
+    DefaultError,
+    #[msg("Offer expired")]
+    OfferExpired,
+    #[msg("This pool is locked")]
+    PoolLocked,
+    #[msg("Slippage exceeded")]
+    SlipageExceeded,
+    #[msg("Overflow detected")]
+    Overflow,
+    #[msg("Underflow detected")]
+    Underflow,
+    #[msg["Invalid Token"]]
+    InvalidToken,
+    #[msg("Actual liquidity is less than minimum.")]
+    LiquidityLessThanMinimum,
+    #[msg("No liquidity in pool.")]
+    NoLiquidityInPool,
+    #[msg("Bump error.")]
+    BumpError,
+    #[msg("Curve Error")]
+    CurveError,
+    #[msg("Fee is greater than 100%. This is not a very good deal.")]
+    InvalidFee,
+    #[msg("Invalid update authority")]
+    InvalidAuthority,
+    #[msg("No update authority set.")]
+    NoAuthoritySet,
+    #[msg("Invalid amount.")]
+    InvalidAmount,
+    #[msg("Invalid precision")]
+    InvalidPrecision,
+    #[msg("Isufficient balance.")]
+    InsufficientBalance,
+    #[msg("Zero Balance.")]
+    ZeroBalance,
+}
