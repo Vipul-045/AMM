d1628d3f617771cb1f6302f2a159e77ebf4e4a78
Vipul-045 <vipulyadav0479@gmail.com>
2025-11-22 16:05:52 +0530

    tests

--- tests/amm.ts
+++ tests/amm.ts
@@ -1,16 +1,4 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Amm } from "../target/types/amm";
+import { Amms } from "../target/types/amms";
 
-describe("amm", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
-  const program = anchor.workspace.amm as Program<Amm>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
